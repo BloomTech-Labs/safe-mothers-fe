85d8f291e2d24248e01b551a4e9010128b383f93
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-10-15 15:06:52 -0400

    added formik login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormikLoginForm from './components/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+
+    <Route
+      exact path="/"
+      render = {props => {
+        return <FormikLoginForm  history={props.history}/>
+        }}
+    />
+    {/* <FormikLoginForm history = {props}/> */}
+
+    </Router>
     </div>
   );
 }
