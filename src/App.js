9be7294b4382c0a3b7cba7d55591baf58330c07d
Blake <bpro0821@gmail.com>
2019-10-16 14:41:42 -0500

    adds basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import List from './components/List'
+import Dashboard from './components/Dashboard'
 import FormikLoginForm from './components/Login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -13,6 +15,18 @@ function App() {
       exact path="/"
       render = {props => {
         return <FormikLoginForm  history={props.history}/>
+        }}
+    />
+    <Route
+      path="/dashboard"
+      render = {props => {
+        return <Dashboard  history={props.history}/>
+        }}
+    />
+    <Route
+      path="/list"
+      render = {props => {
+        return <List history={props.history}/>
         }}
     />
     {/* <FormikLoginForm history = {props}/> */}
