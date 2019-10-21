2de368a5c566fc8241dc87a74eed87ef34595a00
AlexFox1777 <mix.filipov@mail.ru>
2019-10-21 13:09:37 -0700

    done

--- src/App.js
+++ src/App.js
@@ -6,26 +6,29 @@ import FormikLoginForm from './components/Login';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AuthRoute from "./utilities/privateRoute";
 import Registration from "./components/Registration";
+import {ThemeProvider} from 'pcln-design-system'
 
 function App() {
     return (
-        <div className="App">
-            <Router>
-                <Route exact path="/" component={FormikLoginForm}/>
-                <Route path="/registration" component={Registration}/>
-                {/** PRIVATE ROUTE */}
-                <Switch>
-                    <AuthRoute
-                        exact path="/dashboard"
-                        component={Dashboard}
-                    />
-                    <AuthRoute
-                        exact path="/list"
-                        component={List}
-                    />
-                </Switch>
-            </Router>
-        </div>
+        <ThemeProvider>
+            <div className="App">
+                <Router>
+                    <Route exact path="/" component={FormikLoginForm}/>
+                    <Route path="/registration" component={Registration}/>
+                    {/** PRIVATE ROUTE */}
+                    <Switch>
+                        <AuthRoute
+                            exact path="/dashboard"
+                            component={Dashboard}
+                        />
+                        <AuthRoute
+                            exact path="/list"
+                            component={List}
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        </ThemeProvider>
     );
 }
 
