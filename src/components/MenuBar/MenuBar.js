1b784a592454ffffbe606e43c68d9a8ae23435a9
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-10-21 18:39:30 -0400

    Created a MenuBar going to merge to master

---
+++ src/components/MenuBar/MenuBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Banner, Text, Button } from 'pcln-design-system';
+import './Dashboard.css';
+
+const  MenuBar = () => {
+  return (
+    <>
+      <Banner px={10} py={30} bg="#282E74" color="white" showIcon={false}>
+        <div className="BannerContainer">
+          <Text className = "text">Hi, Jackie</Text>
+          <div className="ButtonContainer">
+            <Button className="dashButton" >
+              Mom
+            </Button>
+            <Button className="dashButton" >
+              Driver
+            </Button>
+            <Button className="dashButton" >
+              Settings
+            </Button>
+            <Button className="dashButton">
+              Log Off
+            </Button>
+          </div>
+        </div>
+      </Banner>
+      <Mothers />
+      <Drivers />
+    </>
+  );
+};
+
+export default MenuBar;
+
