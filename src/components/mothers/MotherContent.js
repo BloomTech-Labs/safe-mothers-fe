8131005d8f21dd725a53e7a00b29e35b4727dff2
Blake <bpro0821@gmail.com>
2019-10-23 17:46:47 -0500

    Adds Accordion Shell

---
+++ src/components/mothers/MotherContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default function MotherContent (props) {
+    const {mother} = props
+    return (
+        <>
+       
+        </>
+    )
+}
--- src/components/mothers/Mothers.js
+++ src/components/mothers/Mothers.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getMothers } from '../../actions/mothersActions';
+import { Accordion, AccordionPanel, Box } from 'grommet';
+import AccordionLabel from './AccordionLabel';
+import AccordionContent from './AccordionContent';
 
 const Mothers = props => {
   const { mothers } = props;
@@ -11,19 +14,22 @@ const Mothers = props => {
 
   return (
     <>
-
+      <Box>
+        {console.log(mothers)}
         <h3>List of mothers: </h3>
-        {mothers &&
-          mothers.map((mother, index) => (
-            <p key={index}>
-              Firstname: {mother.first_name}, Lastname: {mother.last_name}, age:{' '}
-              {mother.age}
-            </p>
-          ))}
-
+          <Accordion animate={true} multiple={true} margin='small'>
+            {mothers &&
+            mothers.map((mother, index) => (
+            <AccordionPanel label= {<AccordionLabel mother={mother} />}>
+              <Box background='light-1'><AccordionContent mother={mother} /> </Box>
+            </AccordionPanel>
+            ))}
+          </Accordion>
+      </Box>
     </>
   );
 };
+
 const mapStateToProps = state => {
   return {
     mothers: state.mothersReducer.mothers
