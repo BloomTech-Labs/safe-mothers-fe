00587b0c0e498c57a57b29bdd9cd6d005cc301ee
AlexFox1777 <mix.filipov@mail.ru>
2019-10-28 10:54:22 -0700

    when moving from home page the link was still active, now it is fixed

--- src/components/menubar/MenuBar.js
+++ src/components/menubar/MenuBar.js
@@ -5,7 +5,6 @@ import {Link, withRouter} from "react-router-dom";
 import {Container} from "./menubar-style";
 
 const MenuBar = (props) => {
-    const [active, setActive] = useState();
     const [links, setLinks] = useState({
         dashboard: "/dashboard",
         mothers: "/mothers",
@@ -18,46 +17,51 @@ const MenuBar = (props) => {
             <Container>
                 <div className="links">
                     <div className="link-container">
-                        <Link onClick={() => setActive(1)}
-                              className={"link " + (active === 1 || props.location.pathname === links.dashboard ? "focus-link" : "")}
+                        <Link
+                            className={"link " + (props.location.pathname === links.dashboard ? "focus-link" : "")}
                               to="/dashboard">
-                            Home <div className="dash-button"></div>
-                            <div
-                                className={active === 1 || props.location.pathname === links.dashboard ? "focus" : ""}></div>
+                            Home <div className="dash-button">
+                        </div>
+                            <div className={props.location.pathname === links.dashboard ? "focus" : ""}>
+                            </div>
                         </Link>
                     </div>
                     <div className="link-container">
-                        <Link onClick={() => setActive(2)}
-                              className={"link " + (active === 2 || props.location.pathname === links.mothers ? "focus-link" : "")}
+                        <Link
+                            className={"link " + (props.location.pathname === links.mothers ? "focus-link" : "")}
                               to="/mothers">
-                            Mom <div className="dash-button"></div>
-                            <div
-                                className={active === 2 || props.location.pathname === links.mothers ? "focus" : ""}></div>
+                            Mom <div className="dash-button">
+                        </div>
+                            <div className={props.location.pathname === links.mothers ? "focus" : ""}>
+                            </div>
                         </Link>
                     </div>
                     <div className="link-container">
-                        <Link onClick={() => setActive(3)}
-                              className={"link " + (active === 3 || props.location.pathname === links.drivers ? "focus-link" : "")}
-                              to="/drivers">
-                            Driver <div className="dash-button"></div>
-                            <div
-                                className={active === 3 || props.location.pathname === links.drivers ? "focus" : ""}></div>
+                        <Link
+                            className={"link " + (props.location.pathname === links.drivers ? "focus-link" : "")}
+                            to="/drivers">
+                            Driver <div className="dash-button">
+                        </div>
+                            <div className={props.location.pathname === links.drivers ? "focus" : ""}>
+                            </div>
                         </Link>
                     </div>
                     <div className="link-container">
-                        <Link onClick={() => setActive(4)}
-                              className={"link " + (active === 4 || props.location.pathname === links.settings ? "focus-link" : "")}
-                              to="/settings">
+                        <Link
+                            className={"link " + (props.location.pathname === links.settings ? "focus-link" : "")}
+                            to="/settings">
                             Settings
-                            <div className="dash-button"></div>
-                            <div
-                                className={active === 4 || props.location.pathname === links.settings ? "focus" : ""}></div>
+                            <div className="dash-button">
+                            </div>
+                            <div className={props.location.pathname === links.settings ? "focus" : ""}>
+                            </div>
                         </Link>
                     </div>
                     <div className="link-container">
                         <Link onClick={() => props.logout()} className="link " to="/">
                             Sign Out
-                            <div className="dash-button"></div>
+                            <div className="dash-button">
+                            </div>
                         </Link>
                     </div>
                 </div>
