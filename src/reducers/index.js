643c44cde4a4a06f7a1e005aeb1c5431b382e2f3
AlexFox1777 <mix.filipov@mail.ru>
2019-10-31 10:41:43 -0700

    fixed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,10 @@ import {combineReducers} from "redux";
 import authReducer from "./authReducer";
 import mothersReducer from "./mothersReducer";
 import driversReducer from "./driversReducer";
-import settingsReducer from './settingsReducer';
+// import settingsReducer from './settingsReducer';
 
 export default combineReducers({
     authReducer,
     mothersReducer,
     driversReducer,
-    settingsReducer,
 });
