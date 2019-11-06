3605aef70fc00533d1c1594acd7fd635c64e9ccf
Jonathan Dorety <kyullog@gmail.com>
2019-11-06 15:51:31 -0800

    Revert to localhost if not set in ENV variable

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = process.env.REACT_APP_API_URL
+const api = process.env.REACT_APP_API_URL || "http://localhost:5000"
 
 const axiosWithAuth = () => {
     const token = localStorage.getItem("token");
