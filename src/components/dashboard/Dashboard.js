92a6367a42eecb27abd260112f064bd533d906f1
Jonathan Dorety <kyullog@gmail.com>
2019-11-12 16:39:19 -0800

    Driver info displays

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import moment from "moment";
-import {getMothers} from '../../actions/mothersActions';
-import {connect} from 'react-redux';
-import styled from 'styled-components';
+import { getMothers } from "../../actions/mothersActions";
+import { connect } from "react-redux";
+import styled from "styled-components";
 import DashboardCard from "./DashboardCard";
 
-import {DRIVER, MOTHER} from "./dashboard-utils";
+import { DRIVER, MOTHER } from "./dashboard-utils";
 
 const MainContainer = styled.div`
   display: flex;
@@ -16,10 +16,10 @@ const MainContainer = styled.div`
     width: 30%;
     margin: 20px;
   }
-  .cards{
+  .cards {
     width: 80%;
   }
-  ${'' /* .rankingCard {
+  ${"" /* .rankingCard {
      display: flex;
      width: 50%;
      height: 70%
@@ -29,68 +29,84 @@ const MainContainer = styled.div`
    } */}
 `;
 
-  const Dashboard = props => {
-      useEffect(() => {
-        props.getMothers();
-      }, []);
+const Dashboard = props => {
+  useEffect(() => {
+    props.getMothers();
+  }, []);
 
-    const dueNow = (mothers) => {
-      let num = 0;
-      if(mothers){
-        for (let mother of mothers) {
-        if(mother.due_now === 1){
+  const dueNow = mothers => {
+    let num = 0;
+    if (mothers) {
+      for (let mother of mothers) {
+        if (mother.due_now === 1) {
           num = num + 1;
         }
-         }
-        }
-        return num;
       }
-    
-    const lateDueDay = (mothers) => {
-      let num = 0;
-
-      const today = moment();
-        if(mothers) {
-          for (let mother of mothers) {
-            let dueDate = moment(mother.edd, "YYYY-MM-DD");
-            console.log("dueDate", dueDate, "today", today)
-            if (moment(dueDate).isBefore(today)) num = num + 1;
-          }
-          }
-        return num;
-
-  }
-  const availability = (drivers) => {
-          let num = 7;
-          if(drivers){
-            for (let drivers of drivers) {
-            if(drivers.availability === 1){
-              num = num + 1;
-            }
-             }
-            }
-            return num;
-    
-    
+    }
+    return num;
   };
-return (
-      
-        <MainContainer className="mainContainer">
-            <div className="cards">
-                <DashboardCard val1={dueNow(props.mothers)} val2={lateDueDay(props.mothers)} val3="4" cardState={MOTHER}/>
-                <DashboardCard val1="1" val2={availability(props.drivers)} val3="1"  cardState={DRIVER}/>
-            </div>
-            <div className="board">
-                {/*Driver Rankings*/}
-            </div>
-        </MainContainer>
-    )
+
+  const lateDueDay = mothers => {
+    let num = 0;
+
+    const today = moment();
+    if (mothers) {
+      for (let mother of mothers) {
+        let dueDate = moment(mother.edd, "YYYY-MM-DD");
+        console.log("dueDate", dueDate, "today", today);
+        if (moment(dueDate).isBefore(today)) num = num + 1;
+      }
+    }
+    return num;
+  };
+  const availability = drivers => {
+    let num = 0;
+    if (drivers) {
+      for (let driver of drivers) {
+        if (driver.availability) {
+          num = num + 1;
+        }
+      }
+    }
+    return num;
+  };
+
+  const recent = drivers => {
+    let num = 0;
+    let today = moment();
+    let recentWindow = moment(today).subtract(14, "days");
+    if (drivers) {
+      for (let driver of drivers) {
+        let created = moment(driver.created_at);
+        if (moment(created).isBetween(recentWindow, today)) num += 1;
+      }
+    }
+    return num;
+  };
+  return (
+    <MainContainer className="mainContainer">
+      <div className="cards">
+        <DashboardCard
+          val1={dueNow(props.mothers)}
+          val2={lateDueDay(props.mothers)}
+          val3="4"
+          cardState={MOTHER}
+        />
+        <DashboardCard
+          val1={recent(props.drivers)}
+          val2={availability(props.drivers)}
+          val3="1"
+          cardState={DRIVER}
+        />
+      </div>
+      <div className="board">{/*Driver Rankings*/}</div>
+    </MainContainer>
+  );
 };
 const mapStateToProps = state => {
   return {
-      mothers: state.mothersReducer.mothers,
-      
+    mothers: state.mothersReducer.mothers,
+    drivers: state.driversReducer.drivers
   };
-}
-export default connect(mapStateToProps, {getMothers})(Dashboard);
-
+};
+export default connect(mapStateToProps, { getMothers })(Dashboard);
