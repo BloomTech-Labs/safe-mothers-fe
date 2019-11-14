593e7ae761b54e237d1d5d1b502afb5584add758
Jonathan Dorety <kyullog@gmail.com>
2019-11-14 14:44:18 -0800

    Setup env variable in axios

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -1,16 +1,17 @@
-
-import axios from 'axios';
+import axios from "axios";
+require("dotenv").config();
 
 const axiosWithAuth = () => {
-    const token = localStorage.getItem("token");
-
-    return axios.create({
-        baseURL: "https://staging-be-labs17-safe.herokuapp.com",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: token
-        }
-    });
+  const token = localStorage.getItem("token");
+  const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  console.log(apiUrl);
+  return axios.create({
+    baseURL: apiUrl,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    }
+  });
 };
 
 export default axiosWithAuth;
