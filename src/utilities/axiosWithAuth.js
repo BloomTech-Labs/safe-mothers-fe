1f28c497bf70423362671684bff8761a307f5723
Jonathan Dorety <kyullog@gmail.com>
2019-11-14 14:51:12 -0800

    Add some comments

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
+  // retrieve API base url from .env
   const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
   console.log(apiUrl);
   return axios.create({
+    // sets base url, and authorization headers with each axios request
     baseURL: apiUrl,
     headers: {
       "Content-Type": "application/json",
